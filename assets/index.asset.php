cab369fafcab1d540d06a5dbcc34e3ff8a3172fd
Curtis <cortezcreations@gmail.com>
2023-03-13 10:24:44 -0400

    Run NPM Build

--- assets/index.asset.php
+++ assets/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api', 'wp-api-fetch', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'd70d0e7aa42b1fa305fd');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api', 'wp-api-fetch', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'ece11a04d484b4f29021');
